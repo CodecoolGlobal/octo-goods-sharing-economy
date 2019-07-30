538271634fb754a04e23b833cc891c7bdd57ed58
Liz <pachaffy@gmail.com>
2019-07-30 14:39:40 +0200

    Add templates

---
+++ src/main/java/com/codecool/octogoods/OctoGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.octogoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OctoGoodsApplication.class, args);
+    }
+
+}
