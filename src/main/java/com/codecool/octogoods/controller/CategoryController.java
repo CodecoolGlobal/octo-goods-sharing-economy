21bc14378784ce2d1f635dc470a222bdd0af746b
Liz <pachaffy@gmail.com>
2019-07-31 16:07:20 +0200

    Implement get method in CategoryController

--- src/main/java/com/codecool/octogoods/controller/CategoryController.java
+++ src/main/java/com/codecool/octogoods/controller/CategoryController.java
@@ -1,4 +1,28 @@
 package com.codecool.octogoods.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codecool.octogoods.model.Category;
+import com.codecool.octogoods.service.CategoryService;
+
+@RestController
 public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/categories")
+    public List<Category> getCategories() {
+        return categoryService.getAllCategories();
+    }
+
+
 }
