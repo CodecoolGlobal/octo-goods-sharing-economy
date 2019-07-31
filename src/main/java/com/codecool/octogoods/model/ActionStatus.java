82ef429ac6881c3f61d6ad91812620c67d2db436
mszotek <mszotek@gmail.com>
2019-07-31 13:24:59 +0200

    Create StatusController with Post handler

--- src/main/java/com/codecool/octogoods/model/ActionStatus.java
+++ src/main/java/com/codecool/octogoods/model/ActionStatus.java
@@ -12,6 +12,38 @@ public class ActionStatus {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private boolean availability;
+    private boolean available;
     private boolean isActive;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
