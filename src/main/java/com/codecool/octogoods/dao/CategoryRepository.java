bf52be5d29b49c3303443ed81df628c791695ab3
Liz <pachaffy@gmail.com>
2019-08-01 17:55:05 +0200

    Add findByName method in CategoryRepository

--- src/main/java/com/codecool/octogoods/dao/CategoryRepository.java
+++ src/main/java/com/codecool/octogoods/dao/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.codecool.octogoods.dao;
 
-import com.codecool.octogoods.model.Category;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.codecool.octogoods.model.Category;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+    Optional<Category> findByName(String name);
 }
