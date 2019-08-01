a5b44c9dda38bb6825bb35c3867e4867cfb38609
Liz <pachaffy@gmail.com>
2019-08-01 14:06:47 +0200

    Implement get, put and post mapping in CategoryController

--- src/main/java/com/codecool/octogoods/controller/CategoryController.java
+++ src/main/java/com/codecool/octogoods/controller/CategoryController.java
@@ -2,14 +2,31 @@ package com.codecool.octogoods.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.lang.NonNull;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.server.ResponseStatusException;
 
 import com.codecool.octogoods.model.Category;
 import com.codecool.octogoods.service.CategoryService;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
 
     private CategoryService categoryService;
@@ -19,10 +36,31 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/api/categories")
+    @GetMapping("/categories")
     public List<Category> getCategories() {
         return categoryService.getAllCategories();
     }
 
+    @PostMapping("/category")
+    public ResponseEntity<Category> addCategory(@Valid @NonNull @RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.insertCategory(category), HttpStatus.CREATED);
+    }
 
+    @GetMapping("/category/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") int id) {
+        try {
+            return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PutMapping("/category")
+    public ResponseEntity<Category> updateCategory(@Valid @NonNull @RequestBody Category category) {
+        try {
+            return new ResponseEntity<>(categoryService.updateCategory(category), HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
